b5835558ba71337309647aa0793755bd80bfab51
Haena <ghkwlsspdl@naver.com>
2023-01-07 18:09:02 +0900

    update10773

---
+++ BOJ/10773.swift
@@ -0,0 +1,27 @@
+//
+//  10773.swift
+//  BOJ
+//
+//  Created by leehwajin on 2023/01/07.
+//
+
+import Foundation
+//10773 제로
+var n = Int(readLine()!)!
+var stack = [Int]()
+
+for _ in 0..<n{
+    let input = Int(readLine()!)!
+    if input == 0{
+        stack.remove(at: stack.endIndex-1)
+    }else{
+        stack.append(input)
+    }
+}
+
+var sum = 0
+for i in stack{
+    sum += i
+}
+
+print(sum)
