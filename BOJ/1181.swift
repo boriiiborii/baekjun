e0e85dec8d6757c0abddc335c135d2dc4d6ceed8
Haena <ghkwlsspdl@naver.com>
2023-01-16 01:51:14 +0900

    update1181

--- BOJ/1181.swift
+++ BOJ/1181.swift
@@ -2,46 +2,19 @@
 //  1181.swift
 //  BOJ
 //
-//  Created by leehwajin on 2022/12/28.
+//  Created by leehwajin on 2023/01/16.
 //
 
 import Foundation
-//1181
-
-let times = Int(readLine()!)!
-var stArr = Array(repeating: "", count: times)
-
-for i in 0..<times{
-    stArr[i] = readLine()!
+//1181 실버
+let n = Int(readLine()!)!
+var arr = [String]()
+for _ in 0..<n{
+    arr.append(readLine()!)
 }
-stArr.sort()
+let a = Set(arr)
+arr = Array(a)
 
-for i in 0..<times{
-    if times == 1{
-        break
-    }
-    var temp = ""
-    if stArr[i].count >= stArr[i+1].count{
-        temp = stArr[i+1]
-        stArr[i+1] = stArr[i]
-        stArr[i] = temp
-        if (i == times-2){
-            break
-        }
-    }
-}
-var num = 0
-for i in 0..<stArr.count-1{
-    if stArr[i] == stArr[i+1]{
-        stArr.remove(at: i)
-        num += 1
-    }
-    if i == stArr.count-1-num{
-        break
-    }
-}
+let arrr = arr.sorted{$0.count == $1.count ?  $0 < $1 : $0.count < $1.count}
 
-for i in 0..<stArr.count{
-    print(stArr[i])
-}
-
+arrr.map{print($0)}
