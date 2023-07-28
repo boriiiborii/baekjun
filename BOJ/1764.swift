5d96d6ff15546755a42a22e9d6b980b41d1e5681
Haena <ghkwlsspdl@naver.com>
2023-07-28 20:22:03 +0900

    update 1764

---
+++ BOJ/1764.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+//1764 듣보잡
+
+var dic: [String:Int] = [:]
+var array: [String] = []
+let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+
+let n = input.first
+let m = input.last
+
+for _ in 0..<n! {
+    let noListen = readLine()!
+    dic[noListen] = 1
+}
+
+for _ in 0..<m! {
+    let noSee = readLine()!
+    if dic.keys.contains(noSee) {
+        array.append(noSee)
+    }
+}
+let sortedArray = array.sorted()
+print(sortedArray.count)
+for i in 0..<sortedArray.count {
+    print(sortedArray[i])
+}
+
+
+
