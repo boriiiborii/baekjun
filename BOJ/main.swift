646a51f740643d7a748c44d6e6dc68f3ad2e7687
Haena <ghkwlsspdl@naver.com>
2023-08-12 02:25:11 +0900

    update 17413

--- BOJ/main.swift
+++ BOJ/main.swift
@@ -1,1 +1,37 @@
 import Foundation
+//17413 단어뒤집기2
+
+let input = readLine()!
+var status = false
+var word = ""
+var result = ""
+
+for char in input {
+    if char == "<" {
+        status = true
+        result += word.reversed()
+        word = ""
+    }else if char == " " && status == false {
+        result += word.reversed()
+        result += " "
+        word = ""
+    }
+    
+    switch status {
+    case true:
+        result += String(char)
+    case false:
+        if char != " " {
+            word += String(char)
+        }
+    }
+    if char == ">" {
+        status = false
+    }
+}
+
+if word.count != 0 {
+    result += word.reversed()
+}
+
+print(result)
