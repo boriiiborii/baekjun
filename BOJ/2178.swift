201915342a7cc057b91ff47dfc67bbb799b526aa
Haena <ghkwlsspdl@naver.com>
2023-11-19 19:12:08 +0900

    update 2178코드 수정

--- BOJ/2178.swift
+++ BOJ/2178.swift
@@ -1,10 +1,17 @@
+//
+//  1260.swift
+//  BOJ
+//
+//  Created by leehwajin on 2023/01/09.
+//
 
 import Foundation
 //2178 미로탐색
 let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
 let n = nm[0]
 let m = nm[1]
-var result = 0
+let dx = [0,0,1,-1]
+let dy = [1,-1,0,0]
 
 var map = [[Int]]()
 for _ in 0..<n {
@@ -13,9 +20,7 @@ for _ in 0..<n {
 
 var queue: [(x: Int, y: Int, count: Int)] = [(0,0,1)]
 var index = 0
-
-let dx = [-1, 1, 0, 0]
-let dy = [0, 0, -1, 1]
+var result = 0
 
 while index < queue.count {
     let node = queue[index]
@@ -24,20 +29,17 @@ while index < queue.count {
     for i in 0..<4 {
         let nx = node.x + dx[i]
         let ny = node.y + dy[i]
-        let nCount = node.count+1
+        let nCount = node.count + 1
         
-        if nx >= 0 && ny >= 0 && nx < n && ny < m {
-            if map[nx][ny] == 1 {
-                result = nCount
-                if nx == n-1 && ny == m-1 {
-                    index = queue.count
-                    break
-                }
-                map[nx][ny] = 0
-                queue.append((nx, ny, nCount))
+        if nx >= 0 && ny >= 0 && nx < n && ny < m && map[nx][ny] == 1 {
+            queue.append((nx,ny,nCount))
+            map[nx][ny] = 0
+            result = nCount
+            if nx == n-1 && ny == m-1 {
+                index = queue.count
+                break
             }
         }
     }
 }
-
 print(result)
--- BOJ/main.swift
+++ BOJ/main.swift
@@ -6,43 +6,40 @@
 //
 
 import Foundation
-//1260 DFS와BFS
-var nmv = readLine()!.split(separator: " ").map { Int(String($0))! }
-let n = nmv[0]
-let m = nmv[1]
-let v = nmv[2]
+//2178 미로탐색
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nm[0]
+let m = nm[1]
+let dx = [0,0,1,-1]
+let dy = [1,-1,0,0]
 
-var visited = Array(repeating: false, count: n+1)
-var matrix = Array(repeating: Array(repeating: 0, count: n+1), count: n+1)
-
-for i in 0..<m {
-    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
-    matrix[input[0]][input[1]] = 1
-    matrix[input[1]][input[0]] = 1
+var map = [[Int]]()
+for _ in 0..<n {
+    map.append(readLine()!.map{Int(String($0))!})
 }
 
-func bfs(_ start :Int) {
-    var queue = [start]
-    while !queue.isEmpty {
-        var node = queue.removeFirst()
-        visited[node] = true
-        print(node)
-        for i in 1...n {
-            if visited[i] == false && matrix[i][node] == 1 {
-                visited[i] = true
-                queue.append(i)
+var queue: [(x: Int, y: Int, count: Int)] = [(0,0,1)]
+var index = 0
+var result = 0
+
+while index < queue.count {
+    let node = queue[index]
+    index += 1
+    
+    for i in 0..<4 {
+        let nx = node.x + dx[i]
+        let ny = node.y + dy[i]
+        let nCount = node.count + 1
+        
+        if nx >= 0 && ny >= 0 && nx < n && ny < m && map[nx][ny] == 1 {
+            queue.append((nx,ny,nCount))
+            map[nx][ny] = 0
+            result = nCount
+            if nx == n-1 && ny == m-1 {
+                index = queue.count
+                break
             }
         }
     }
 }
-
-func dfs(_ start: Int) {
-    visited[start] = true
-    print(start, terminator: " ")
-    for i in 1..<n+1 {
-        if visited[i] == false && matrix[start][i] == 1 {
-            dfs(i)
-        }
-    }
-}
-
+print(result)
