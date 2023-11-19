521f4424d84e64f79ca201af5f06c50c47cc65ba
Haena <ghkwlsspdl@naver.com>
2023-11-19 20:47:44 +0900

    update 2667 bfs 추가

--- BOJ/2667.swift
+++ BOJ/2667.swift
@@ -46,3 +46,61 @@ private func dfs(x: Int, y: Int) {
 print(result.count)
 let sortedResult = result.sorted()
 sortedResult.forEach{print($0)}
+
+////bfs코드
+//import Foundation
+////2667 단지번호붙이기
+//var n = Int(readLine()!)!
+//var map = [[Int]]()
+//let dx = [0, 0, 1, -1]
+//let dy = [1, -1, 0, 0]
+//var queue = [(x: Int, y: Int)]()
+//var count = 0
+//var result = [Int]()
+//
+//
+//for _ in 0..<n {
+//    map.append(readLine()!.map { Int(String($0))! })
+//}
+//
+//
+//for i in 0..<n {
+//    for j in 0..<n {
+//        if map[i][j] == 1 {
+//            let componentSize = bfs(i, j)
+//            count += 1
+//            result.append(componentSize)
+//        }
+//    }
+//}
+//
+//func bfs(_ x: Int, _ y: Int) -> Int {
+//    var count = 0
+//    queue = [(x, y)]
+//
+//    while !queue.isEmpty {
+//        let node = queue.removeFirst()
+//        let x = node.x
+//        let y = node.y
+//
+//        if map[x][y] == 1 {
+//            map[x][y] = 0
+//            count += 1
+//
+//            for i in 0..<4 {
+//                let nx = x + dx[i]
+//                let ny = y + dy[i]
+//
+//                if nx >= 0 && ny >= 0 && nx < n && ny < n && map[nx][ny] == 1 {
+//                    queue.append((nx, ny))
+//                }
+//            }
+//        }
+//    }
+//
+//    return count
+//}
+//
+//print(result)
+//print(count)
+//
