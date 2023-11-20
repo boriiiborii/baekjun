ca9df9c7574ea35e476b4805abccb2e31d3f508f
Haena <ghkwlsspdl@naver.com>
2023-11-20 22:32:19 +0900

    update 1012 bfs추가

--- BOJ/1012.swift
+++ BOJ/1012.swift
@@ -48,3 +48,57 @@ for _ in 0..<times {
     print(count)
 }
 
+//import Foundation
+////1012 유기농배추
+//
+//var map = [[Int]]()
+//var mnk = [Int]()
+//for _ in 0..<Int(readLine()!)! {
+//    var result = 0
+//    mnk = readLine()!.split(separator: " ").map{Int(String($0))!}
+//    map = Array(repeating: Array(repeating: 0, count: mnk[1]), count: mnk[0])
+//    
+//    for _ in 0..<mnk[2] {
+//        let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+//        map[input[0]][input[1]] = 1
+//    }
+//    
+//    for i in 0..<mnk[0] {
+//        for j in 0..<mnk[1] {
+//            if map[i][j] == 1 {
+//                bfs(i,j)
+//                result += 1
+//            }
+//        }
+//    }
+//    
+//    print(result)
+//    
+//}
+//
+//
+//func bfs(_ x: Int, _ y:Int) {
+//    let dx = [0,0,1,-1]
+//    let dy = [1,-1,0,0]
+//    
+//    var queue = [(x: x,y: y)]
+//    
+//    while !queue.isEmpty {
+//        let node = queue.removeFirst()
+//        let x = node.x
+//        let y = node.y
+//        
+//        if map[x][y] == 1 {
+//            map[x][y] = 0
+//            
+//            for i in 0..<4 {
+//                let nx = node.x+dx[i]
+//                let ny = node.y+dy[i]
+//                
+//                if nx >= 0 && ny >= 0 && nx < mnk[0] && ny < mnk[1] && map[nx][ny] == 1 {
+//                    queue.append((nx,ny))
+//                }
+//            }
+//        }
+//    }
+//}
