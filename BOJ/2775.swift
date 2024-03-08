34accc881955dc2db0b4b39f3462681ef22f1c25
Haena <ghkwlsspdl@naver.com>
2024-03-08 22:37:49 +0900

    update 2775

--- BOJ/2775.swift
+++ BOJ/2775.swift
@@ -22,3 +22,22 @@ for _ in 0..<n{
     }
     print(arr[k][n-1])
 }
+
+import Foundation
+//2775
+let times = Int(readLine()!)!
+for _ in 0..<times {
+    var dp = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]]
+    let k = Int(readLine()!)!
+    let n = Int(readLine()!)!
+    
+    for i in 1...k {
+        var arr = [0]
+        for j in 1...n {
+            arr.append(arr[j-1]+dp[i-1][j])
+        }
+        dp.append(arr)
+    }
+    print(dp[k][n])
+}
+
