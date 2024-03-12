63c763eadc5833c19936f500d89d93de58d49ae8
Haena <ghkwlsspdl@naver.com>
2024-03-12 19:20:47 +0900

    update 11651

--- BOJ/11651.swift
+++ BOJ/11651.swift
@@ -19,3 +19,18 @@ arr.sort{$0[1]==$1[1] ? $0[0]<$1[0] : $0[1]<$1[1] }
 for i in arr{
     print(i[0], i[1])
 }
+
+
+import Foundation
+//11651
+var arr = [[Int]]()
+for i in 0..<Int(readLine()!)! {
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
+}
+arr.sort{$0[1] == $1[1] ? $0[0] < $1[0] : $0[1] < $1[1]}
+for i in 0..<arr.count {
+    for j in 0..<2 {
+        print(arr[i][j], terminator: " ")
+    }
+    print("")
+}
