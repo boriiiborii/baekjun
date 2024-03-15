0ac88f8d4c66be3f8132b80ad546a2827485a40f
Haena <ghkwlsspdl@naver.com>
2024-03-15 17:52:27 +0900

    update 2164

--- BOJ/2164.swift
+++ BOJ/2164.swift
@@ -21,3 +21,28 @@ else{
         temp += 2
     }
 }
+
+import Foundation
+//2164 이렇게 하면 타임오버(removeFirst가 시간을 많이 잡아먹는다고 듣긴 했다.)
+var queue = [Int](1...Int(readLine()!)!)
+while queue.count != 1 {
+    queue.removeFirst()
+    let ele = queue.removeFirst()
+    queue.append(ele)
+}
+print(queue.first!)
+
+//새로운방법
+var queue = [Int](1...Int(readLine()!)!)
+var temp = 0
+if queue == [1] {
+    print(1)
+}else {
+    while queue[queue.count-2] != 0 {
+        queue[temp] = 0
+        queue.append(queue[temp+1])
+        queue[temp+1] = 0
+        temp += 2
+    }
+    print(queue.last!)
+}
