4e6d7400e3403217a54451d63837d233f87f2f0a
Haena <ghkwlsspdl@naver.com>
2024-03-21 15:20:36 +0900

    update 11723 최선

--- BOJ/11723.swift
+++ BOJ/11723.swift
@@ -1,39 +1,34 @@
+//
+//  11723.swift
+//  BOJ
+//
+//  Created by leehwajin on 2024/03/21.
+//
+
 import Foundation
+//11723
+var set = Set<Int>()
 
-func main() {
-    var s = Array(repeating: 0, count: 21)
-    let times = Int(readLine()!)!
-    var x = 0
-
-    for _ in 0..<times {
-        let input = readLine()!.split(separator: " ").map{String($0)}
-
-        let method = input[0]
-        if input.count == 2 {
-            x = Int(input[1])!
+for _ in 0..<Int(readLine()!)! {
+    let input = readLine()!.split(separator: " ").map { String($0) }
+    switch input[0] {
+    case "add":
+        set.insert(Int(input[1])!)
+    case "remove":
+        set.remove(Int(input[1])!)
+    case "check":
+        print(set.contains(Int(input[1])!) ? 1 : 0)
+    case "toggle":
+        if set.contains(Int(input[1])!) {
+            set.remove(Int(input[1])!)
+        } else {
+            set.insert(Int(input[1])!)
         }
-
-
-        switch method {
-        case "add" :
-            s[x] = 1
-        case "remove" :
-            s[x] = 0
-        case "check" :
-            print(s[x])
-        case "toggle" :
-            if s[x] == 1 {
-                s[x] = 0
-            } else {
-                s[x] = 1
-            }
-        case "all":
-            s = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-        case "empty":
-            s[1...20] = [0]
-        default:
-            break
-        }
+    case "all":
+        set = Set(1...20)
+    case "empty":
+        set.removeAll()
+    default:
+        break
     }
 }
-
