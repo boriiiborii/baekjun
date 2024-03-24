87d6e5f89384083669aff2bbb8118b39d3495dff
Haena <ghkwlsspdl@naver.com>
2024-03-25 00:26:26 +0900

    update 1003

--- BOJ/1003.swift
+++ BOJ/1003.swift
@@ -18,3 +18,17 @@ for _ in 0..<times {
     let number = Int(readLine()!)!
     print("\(arr[number].0) \(arr[number].1)")
 }
+
+
+//1003
+var dp = [[1,0], [0,1]]
+let times = Int(readLine()!)!
+for i in 2...40 {
+    let zero = dp[i-1][0] + dp[i-2][0]
+    let one = dp[i-1][1] + dp[i-2][1]
+    dp.append([zero, one])
+}
+for _ in 0..<times {
+    let input = Int(readLine()!)!
+    print(dp[input][0], dp[input][1], separator: " ")
+}
