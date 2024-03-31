66a9c392dc95af507f7f7a157863c0460a65c7d0
Haena <ghkwlsspdl@naver.com>
2024-03-31 12:47:41 +0900

    update 9095

--- BOJ/9095.swift
+++ BOJ/9095.swift
@@ -16,3 +16,27 @@ for _ in 0..<n {
     let input = Int(readLine()!)!
     print(dp[input])
 }
+
+import Foundation
+//9095
+let times = Int(readLine()!)!
+
+for _ in 0..<times {
+    let input = Int(readLine()!)!
+    if input < 4 {
+        let dp = [0, 1, 2, 4]
+        print(dp[input])
+    }else {
+        var dp = Array(repeating: 0, count: input+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 4
+        for i in 4...input {
+            dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
+        }
+        print(dp[input])
+    }
+}
+    
+
