bdaa19d3eb96f6c6a092da7a871010b4060348ce
Haena <ghkwlsspdl@naver.com>
2024-03-31 14:06:35 +0900

    update 9461

--- BOJ/9461.swift
+++ BOJ/9461.swift
@@ -18,3 +18,18 @@ for _ in 0..<n {
     let input = Int(readLine()!)!
     print(dp[input])
 }
+
+import Foundation
+//9461
+for _ in 0..<Int(readLine()!)! {
+    let input = Int(readLine()!)!
+    var arr = [1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
+    if input < arr.count {
+        print(arr[input-1])
+    }else {
+        for i in 10...input {
+            arr.append(arr[i-3]+arr[i-2])
+        }
+        print(arr[input-1])
+    }
+}
